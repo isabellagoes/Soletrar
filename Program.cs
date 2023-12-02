ead99d47ae49328e217dbbe256d9488a8c86d8f5
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-12-01 21:57:55 -0300

    Soletrando

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string palavraDigitada, palavraSoletrada;
+
+Console.WriteLine("--- Soletrar ---\n");
+
+Console.Write("Digite uma palavra: ");
+palavraDigitada = Console.ReadLine()!;
+
+var caracteres = palavraDigitada.ToCharArray();
+palavraSoletrada = String.Join('-', caracteres).ToUpper();
+
+Console.WriteLine($"\nSoletrando \"{palavraDigitada.ToUpper()}\": {palavraSoletrada}");
